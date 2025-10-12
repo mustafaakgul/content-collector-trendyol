5d3951d3005a56f951b55193de6a5b9a4f1bb799
Mustafa Akgül <mustafaakguldev@icloud.com>
2025-10-12 14:55:19 +0300

    Upd base

---
+++ utils/get_questions.py
@@ -0,0 +1,38 @@
+import requests
+import base64
+
+
+def trendyol_get_questions(api_key, api_secret, seller_id, params):
+    auth_str = f"{api_key}:{api_secret}"
+    auth_bytes = auth_str.encode("utf-8")
+    auth_base64 = base64.b64encode(auth_bytes).decode("utf-8")
+
+    headers = {
+        "Authorization": f"Basic {auth_base64}",
+        "User-Agent": f"{seller_id} - SelfIntegration",
+        "Content-Type": "application/json"
+    }
+
+    url = f"https://apigw.trendyol.com/integration/qna/sellers/{seller_id}/questions/filter"
+
+    response = requests.get(url, headers=headers, params=params)
+
+    result = response.json()
+
+    print(f"Toplam soru sayısı: {result['totalElements']}")
+    print(f"Sayfa: {result['page']} / {result['totalPages']}")
+
+    questions = []
+    for soru in result['content']:
+        print(f"creationDate: {soru['creationDate']}")
+        print(f"customerId: {soru['customerId']}")
+        print(f"id: {soru['id']}")
+        print(f"Ürün: {soru['productName']}")
+        print(f"Soru: {soru['text']}")
+        print(f"Cevap: {soru.get('answer', {}).get('text', 'Cevap yok')}")
+        print(f"Durum: {soru['status']}")
+        print(f"Link: {soru['webUrl']}")
+        print("-" * 40)
+        questions.append(soru)
+
+    return questions
---
+++ utils/request_for_answer.py
@@ -0,0 +1,39 @@
+import requests
+import base64
+from datetime import datetime, timedelta
+import time
+import os
+from dotenv import load_dotenv
+import json
+
+
+load_dotenv()
+
+
+def send_to_other_api(questions):
+    other_api_url = os.getenv("OTHER_API_URL", "http://127.0.0.1:8000/api/v1/comment/")
+    other_api_token = os.getenv("OTHER_API_TOKEN", "ornek_token")
+    headers = {
+        "Content-Type": "application/json",
+        # "Authorization": f"Bearer {other_api_token}"
+    }
+    print(questions)
+    for soru in questions:
+        payload = {
+            "customer_id": soru.get("customerId"),
+            "product_name": soru.get("productName"),
+            "content_id": soru.get("id"),
+            "content": soru.get("text"),
+            "web_url": soru.get("webUrl"),
+            "response": "test",
+            "status": soru.get("status")
+        }
+        try:
+            response = requests.post(other_api_url, headers=headers, data=json.dumps(payload))
+            if response.status_code in [200, 201]:
+                print(f"Soru {payload['content_id']} başarıyla gönderildi. Yanıt: {response.status_code}")
+            else:
+                print(f"Soru {payload['content_id']} gönderilemedi. Hata kodu: {response.status_code}")
+                print(f"Hata mesajı: {response.text}")
+        except Exception as e:
+            print(f"Soru {payload['content_id']} gönderilirken hata oluştu: {str(e)}")
